5ef498dee813cccd4ad2643eadf396b99a19f01a
AlfredLuan <alfred.luan@kii.com>
2017-08-14 16:56:42 +0800

    add aggregation functions

--- js/common.js
+++ js/common.js
@@ -35,7 +35,7 @@ var UserRole = {
 var ShopRole = {
     Head : "head",
     Branch : "branch"
-}
+};
 
 var OrderStatus = {
     OrderPlaced: 0, // order placed by customer
@@ -44,6 +44,12 @@ var OrderStatus = {
     CoffeeReady: 3, // coffee ready by coffee maker
     InDelivery: 4, // coffee picked up by driver
     OrderCompleted: 5 // coffee delivered by driver
+};
+
+var UserAttribute = {
+    Role: "role",
+    Online: "online",
+    Approved: "approved",
 };
 
 var Bucket = {
@@ -70,7 +76,7 @@ var Topic = {
 var PushMessageEvent = {
     OrderUpdated: "order_updated",
     OrderDeleted: "order_deleted"
-}
+};
 
 /////////////////////////////////////////////////////
 // global variables
@@ -419,4 +425,201 @@ function convertArray(array, convert) {
     }
 
     return result;
+}
+
+
+
+/////////////////////////////////////////////////////
+// aggregation
+/////////////////////////////////////////////////////
+
+function count(arr, fieldName) {
+    if (arr === undefined || arr == null) {
+        return 0;
+    }
+    return arr.length;
+}
+
+function sum(arr, fieldName) {
+    if (arr === undefined || arr == null) {
+        return undefined;
+    }
+    var length = arr.length;
+    var sumValue = 0;
+    for (var i = 0; i < length; i++) {
+        if (arr[i][fieldName] !== undefined && arr[i][fieldName] != null) {
+            sumValue += Number(arr[i][fieldName]);
+        }
+    };
+    return sumValue.toFixed(2);
+}
+
+function avg(arr, fieldName) {
+    if (arr === undefined || arr == null || arr.length == 0) {
+        return undefined;
+    }
+    var length = arr.length;
+    var sumValue = 0;
+    for (var i = 0; i < length; i++) {
+        if (arr[i][fieldName] !== undefined && arr[i][fieldName] != null) {
+            sumValue += Number(arr[i][fieldName]);
+        }
+    };
+    return (sumValue / length).toFixed(2);
+}
+
+/**
+ * group recordSet by the specified method "getGroupID"
+ *
+ * for example,
+ *  var getGroupID = function(object) {
+ *    return object["app id"];
+ *  };
+ *
+ * var arr = [{"app id":"BBB", "score":333, "number" : 333},
+ *            {"app id":"AAA", "score":111, "number" : 111},
+ *            {"app id":"AAA"},
+ *            {"app id":"CCC", "score":999, "number" : 999},
+ *            {"app id":"AAA", "score":222, "number" : 222}
+ *            ];
+ *
+ * var result = group(arr, getGroupID);
+ *
+ * the output will be :
+ *  {
+ *     "BBB" : [{"app id":"BBB", "score":333, "number" : 333}],
+ *     "AAA" : [{"app id":"AAA", "score":111, "number" : 111},
+ *              {"app id":"AAA"},
+ *              {"app id":"AAA", "score":222, "number" : 222}],
+ *     "CCC" : [{"app id":"CCC", "score":999, "number" : 999}]
+ *  }
+ *
+ */
+function group(recordSet, getGroupID) {
+    var groupResult = {};
+    var length = recordSet.length;
+    for (var i = 0; i < length; i++) {
+        // get group id
+        var groupID = getGroupID(recordSet[i]);
+        // add recordSet[i] to the corresponding group result
+        if (groupResult[groupID] === undefined) {
+            groupResult[groupID] = [];
+        }
+        groupResult[groupID].push(recordSet[i]);
+    };
+    return groupResult;
+}
+
+/**
+ * group recordSet by the specified method "getGroupID", and aggregate based on "aggregateFieldAndFormula"
+ *
+ * aggregateFieldAndFormula: the array of field and formula to be aggregated, in the format of
+ *   [
+ *     [field1, formula1],
+ *     [field2, formula2],
+ *     ...
+ *   ]
+ *
+ * the output will be in the format of
+ *   [
+ *     {
+ *       groupID: id1,
+ *       aggregationResult: { field1_formula1 : value1, field2_formula2 : value2, ... }
+ *     }, {
+ *       groupID: id2,
+ *       aggregationResult: { field1_formula1 : value1, field2_formula2 : value2, ... }
+ *     },
+ *      ...
+ *   ]
+ *
+ * for example,
+ *  var getGroupID = function(object) {
+ *    return object["app id"];
+ *  };
+ *
+ * var recordSet = [
+ *            {"app id":"BBB", "score":333, "number" : 333},
+ *            {"app id":"AAA", "score":111, "number" : 111},
+ *            {"app id":"AAA"},
+ *            {"app id":"CCC", "score":999, "number" : 999},
+ *            {"app id":"AAA", "score":222, "number" : 222}
+ *          ];
+ *
+ * var aggregateFieldAndFormula = [
+ *            ["score", "sum"],
+ *            ["number","avg"],
+ *          ];
+ *
+ * var result = aggregate(recordSet, getGroupID, aggregateFieldAndFormula);
+ *
+ * the output will be :
+ *   [
+ *     {
+ *       groupID: "BBB",
+ *       aggregationResult: { "score_sum" : 333, "number" : 333 }
+ *     }, {
+ *       groupID: "AAA",
+ *       aggregationResult: { "score_sum" : 333, "number" : 111 }
+ *     }, {
+ *       groupID: "CCC",
+ *       aggregationResult: { "score_sum" : 999, "number" : 999 }
+ *     },
+ *   ]
+ *
+ */
+function aggregate(recordSet, getGroupID, aggregateFieldAndFormula) {
+
+    var supportedFormula = {
+        "sum": sum,
+        "avg": avg,
+        "count": count
+    };
+
+    /**
+     * aggregate the value of each field
+     *
+     * the output will be in the format of
+     *   {
+     *     field1_formula1 : value1,
+     *     field2_formula2 : value2,
+     *     ...
+     *   }
+     * objectArr: the object array with the same group id
+     * aggregateFieldAndFormula: the array of field and formula to be aggregated, in the format of
+     *   [
+     *     [field1,formula1],
+     *     [field2,formula2],
+     *     ...
+     *   ]
+     **/
+    var aggregateForSingleGroup = function(objectArr, aggregateFieldAndFormula) {
+        var aggregateResult = {};
+        for (var i = 0; i < aggregateFieldAndFormula.length; i++) {
+            // get field to be aggregated
+            var field = aggregateFieldAndFormula[i][0];
+            // get formula
+            var formulaName = aggregateFieldAndFormula[i][1];
+            var formula = supportedFormula[formulaName];
+            // calculate value by the formula
+            var value = formula(objectArr, field);
+            aggregateResult[field + "_" + formulaName] = value;
+        };
+        return aggregateResult;
+    };
+    // result array
+    var resultArr = [];
+    // group recordSet by the method "getGroupID"
+    var groupResult = group(recordSet, getGroupID);
+    // calculate the value of each group
+    var groupIDs = Object.keys(groupResult);
+    for (var i = 0; i < groupIDs.length; i++) {
+        var temp = {};
+        // set the groupByField
+        temp["groupID"] = groupIDs[i];
+        // aggregate the value of each field
+        var objectArr = groupResult[groupIDs[i]];
+        temp["aggregationResult"] = aggregateForSingleGroup(objectArr, aggregateFieldAndFormula);
+        resultArr.push(temp);
+    };
+    return resultArr;
 }
